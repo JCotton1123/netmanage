18fabff61ed53204822de2ccfbf456c043241151
root <root@netmanage3.local>
2014-08-21 16:15:41 +0000

    Add updated schema

--- Config/Schema/init.sql
+++ Config/Schema/init.sql
@@ -16,22 +16,30 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `dashboard_stats`
+-- Table structure for table `client_ports`
 --
 
-DROP TABLE IF EXISTS `dashboard_stats`;
+DROP TABLE IF EXISTS `client_ports`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `dashboard_stats` (
-  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
-  `var` varchar(255) NOT NULL,
-  `val` text,
-  `created` datetime DEFAULT NULL,
-  `updated` datetime DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `dashboard_stats_var_idx` (`var`)
+CREATE TABLE `client_ports` (
+  `mac_addr` bigint(20) unsigned NOT NULL,
+  `device_ip_addr` int(11) unsigned NOT NULL,
+  `port` int(11) unsigned NOT NULL,
+  `vlan` int(4) unsigned NOT NULL,
+  `up_down` tinyint(1) DEFAULT '0',
+  `created` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `client_ports`
+--
+
+LOCK TABLES `client_ports` WRITE;
+/*!40000 ALTER TABLE `client_ports` DISABLE KEYS */;
+/*!40000 ALTER TABLE `client_ports` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `device_attr_oids`
@@ -47,8 +55,18 @@ CREATE TABLE `device_attr_oids` (
   `oid` varchar(512) NOT NULL,
   `filter` text,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `device_attr_oids`
+--
+
+LOCK TABLES `device_attr_oids` WRITE;
+/*!40000 ALTER TABLE `device_attr_oids` DISABLE KEYS */;
+INSERT INTO `device_attr_oids` VALUES (1,'1.3.6.1.4.1.9','name','1.3.6.1.2.1.1.5.0','/^([^.]+)\\./'),(2,'1.3.6.1.4.1.9','serial','1.3.6.1.4.1.9.3.6.3.0',NULL),(3,'1.3.6.1.4.1.9','firmware','1.3.6.1.4.1.9.2.1.73.0','/\\/?([^:\\/]+)\\.bin$/'),(4,'1.3.6.1.4.1.9','model','1.3.6.1.2.1.47.1.1.1.1.13.1001',NULL),(5,'1.3.6.1.4.1.9.1.283','model','1.3.6.1.2.1.47.1.1.1.1.13.1',NULL),(6,'1.3.6.1.4.1.9.1.429','model','1.3.6.1.2.1.47.1.1.1.1.13.1',NULL),(7,'1.3.6.1.4.1.9.1.366','model','1.3.6.1.2.1.47.1.1.1.1.13.1',NULL),(8,'1.3.6.1.4.1.9.1.367','model','1.3.6.1.2.1.47.1.1.1.1.13.1',NULL),(9,'1.3.6.1.4.1.9.1.876','model','1.3.6.1.2.1.47.1.1.1.1.13.1',NULL),(10,'1.3.6.1.4.1.9.5.59','serial','1.3.6.1.4.1.9.5.1.2.19.0',NULL),(11,'1.3.6.1.4.1.9.5.59','firmware','1.3.6.1.4.1.9.5.1.1.38.0','/\\/?([^:\\/]+)\\.bin$/'),(12,'1.3.6.1.4.1.9.5.59','model','1.3.6.1.2.1.47.1.1.1.1.13.1',NULL),(13,'1.3.6.1.4.1.9.1.427','model','1.3.6.1.2.1.47.1.1.1.1.13.1',NULL),(14,'1.3.6.1.4.1.9.1.559','model','1.3.6.1.2.1.47.1.1.1.1.13.1',NULL),(15,'1.3.6.1.4.1.9.1.283','firmware','1.3.6.1.4.1.9.2.1.73.0','/^sup-bootflash:(.+)\\.bin$/'),(16,'1.3.6.1.4.1.9.1.1011','model','1.3.6.1.2.1.47.1.1.1.1.13.1',NULL),(17,'1.3.6.1.4.1.9.1.1358','model','1.3.6.1.2.1.47.1.1.1.1.13.1',NULL),(18,'1.3.6.1.4.1.9.1.1525','model','1.3.6.1.2.1.47.1.1.1.1.13.1',NULL);
+/*!40000 ALTER TABLE `device_attr_oids` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `device_configs`
@@ -65,8 +83,17 @@ CREATE TABLE `device_configs` (
   `created` datetime DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `device_configs_device_ip_addr_idx` (`device_ip_addr`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `device_configs`
+--
+
+LOCK TABLES `device_configs` WRITE;
+/*!40000 ALTER TABLE `device_configs` DISABLE KEYS */;
+/*!40000 ALTER TABLE `device_configs` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `device_logs`
@@ -85,8 +112,17 @@ CREATE TABLE `device_logs` (
   KEY `device_logs_timestamp_idx` (`timestamp`),
   KEY `device_logs_device_ip_addr_idx` (`device_ip_addr`),
   KEY `device_logs_fac_sev_mnem_idx` (`fac_sev_mnem`)
-) ENGINE=InnoDB AUTO_INCREMENT=53379 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `device_logs`
+--
+
+LOCK TABLES `device_logs` WRITE;
+/*!40000 ALTER TABLE `device_logs` DISABLE KEYS */;
+/*!40000 ALTER TABLE `device_logs` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `device_neighbors`
@@ -109,8 +145,17 @@ CREATE TABLE `device_neighbors` (
   PRIMARY KEY (`id`),
   KEY `device_neighbors_device_ip_addr_idx` (`device_ip_addr`),
   KEY `device_neighbors_neighbor_ip_idx` (`neighbor_ip_addr`)
-) ENGINE=InnoDB AUTO_INCREMENT=4584 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `device_neighbors`
+--
+
+LOCK TABLES `device_neighbors` WRITE;
+/*!40000 ALTER TABLE `device_neighbors` DISABLE KEYS */;
+/*!40000 ALTER TABLE `device_neighbors` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `devices`
@@ -136,6 +181,42 @@ CREATE TABLE `devices` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
+-- Dumping data for table `devices`
+--
+
+LOCK TABLES `devices` WRITE;
+/*!40000 ALTER TABLE `devices` DISABLE KEYS */;
+/*!40000 ALTER TABLE `devices` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `metrics`
+--
+
+DROP TABLE IF EXISTS `metrics`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `metrics` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `var` varchar(255) NOT NULL,
+  `val` text,
+  `created` datetime DEFAULT NULL,
+  `updated` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `dashboard_stats_var_idx` (`var`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `metrics`
+--
+
+LOCK TABLES `metrics` WRITE;
+/*!40000 ALTER TABLE `metrics` DISABLE KEYS */;
+/*!40000 ALTER TABLE `metrics` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `netmanage_logs`
 --
 
@@ -148,8 +229,17 @@ CREATE TABLE `netmanage_logs` (
   `type` varchar(255) DEFAULT 'info',
   `message` text,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=32355 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `netmanage_logs`
+--
+
+LOCK TABLES `netmanage_logs` WRITE;
+/*!40000 ALTER TABLE `netmanage_logs` DISABLE KEYS */;
+/*!40000 ALTER TABLE `netmanage_logs` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `roles`
@@ -160,13 +250,23 @@ DROP TABLE IF EXISTS `roles`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `roles` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
-  `role` varchar(100) NOT NULL,
-  `descr` varchar(512) DEFAULT NULL,
+  `name` varchar(255) NOT NULL,
+  `description` varchar(255) NOT NULL,
   `updated` datetime DEFAULT NULL,
   `created` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `roles`
+--
+
+LOCK TABLES `roles` WRITE;
+/*!40000 ALTER TABLE `roles` DISABLE KEYS */;
+INSERT INTO `roles` VALUES (1,'administrator','Can perform all actions','2014-08-21 01:13:29','2014-08-21 01:13:29'),(2,'user','Restricted to view only actions','2014-08-21 01:13:29','2014-08-21 01:13:29');
+/*!40000 ALTER TABLE `roles` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `settings`
@@ -184,8 +284,18 @@ CREATE TABLE `settings` (
   `updated` datetime DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `setings_var_idx` (`var`)
-) ENGINE=InnoDB AUTO_INCREMENT=23 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=26 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `settings`
+--
+
+LOCK TABLES `settings` WRITE;
+/*!40000 ALTER TABLE `settings` DISABLE KEYS */;
+INSERT INTO `settings` VALUES (12,'global.snmp_community','public','The SNMP read/write community NetManage will use to interface with your devices.',NULL,NULL),(13,'global.snmp_timeout','3000000','The number of microseconds NetManage will wait for an SNMP response from a device.',NULL,NULL),(14,'global.snmp_retry','5','The number of times NetManage will retry an SNMP operation after a timeout has occurred.',NULL,NULL),(15,'discovery.seeds','127.0.0.1','A comma seperated list of devices NetManage will seed the discovery process with.',NULL,NULL),(16,'discovery.ip_blacklist',NULL,'If this setting is defined, NetManage will not discover devices matching this PCRE filter. This filter supersedes the whitelist filter.',NULL,NULL),(17,'discovery.ip_whitelist','/^.*$/','If this setting is defined, NetManage will only discover devices matching this PCRE filter',NULL,NULL),(18,'device_logging.max_logs_before_term','200','An instance of the LogShell will process this many logs before terminating. The syslog daemon will start a new instance of the LogShell after termination automatically. This is meant as a workaround to address memory leaks.',NULL,NULL),(19,'device_logging.enable_notifications','0','A boolean indicating whether device log messages should be sent out as notifications. Before you enable this you should set the notification filter.',NULL,NULL),(20,'device_logging.notification_recipients','root@netmanage.local','Device log notifications will be sent to this comma separated list of recipients',NULL,NULL),(21,'device_logging.notification_filter','/(LINEPROTO-5-UPDOWN)|(LINK-3-UPDOWN)|(VQPCLIENT-3-THROTTLE)|(VQPCLIENT-3-VLANNAME)|(SYS-2-MALLOCFAIL)|(ILPOWER-5-POWER_GRANTED)|(ILPOWER-5-IEEE_DISCONNECT)|(LINK-5-CHANGED)|(DOT1X-5)|(AUTHMGR-5)|(MAB-5)|(ILPOWER-3-CONTROLLER_PORT_ERR)/','Device logs matching this PCRE filter will not be sent as notifications.',NULL,NULL),(22,'device_logging.storage_filter','/(LINEPROTO-5-UPDOWN)|(LINK-3-UPDOWN)|(LINK-5-CHANGED)/','Logs matching the supplied PCRE filter will not be stored.',NULL,NULL),(23,'tftp.root','/var/lib/tftpboot','Local tftp directory root',NULL,NULL),(24,'tftp.address','127.0.0.1','The local address the tftp process is listening on',NULL,NULL),(25,'config_backup.worker_count','4','Controls the number of workers that are spawned to retrieve your devices configurations.',NULL,NULL);
+/*!40000 ALTER TABLE `settings` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `users`
@@ -196,14 +306,28 @@ DROP TABLE IF EXISTS `users`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `users` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `role_id` int(11) unsigned NOT NULL,
   `username` varchar(50) DEFAULT NULL,
-  `password` char(50) DEFAULT NULL,
+  `password` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `first_name` varchar(255) NOT NULL,
+  `last_name` varchar(255) NOT NULL,
   `updated` datetime DEFAULT NULL,
   `created` datetime DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `users_username_idx` (`username`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,1,'admin','$2a$10$X8e1lXBqBlUZkpEuMT1adul6C2P/it.M7ECZBg5MXjdLGJxVz3/VG','','Builtin','Administrator','2014-08-20 19:47:34','2014-07-12 21:12:39');
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -214,4 +338,4 @@ CREATE TABLE `users` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2014-08-09 21:31:33
+-- Dump completed on 2014-08-21 15:49:07
---
+++ Config/Schema/schema.php
@@ -0,0 +1,168 @@
+<?php 
+class NetmanageSchema extends CakeSchema {
+
+	public function before($event = array()) {
+		return true;
+	}
+
+	public function after($event = array()) {
+	}
+
+	public $client_ports = array(
+		'mac_addr' => array('type' => 'biginteger', 'null' => false, 'default' => null, 'unsigned' => true),
+		'device_ip_addr' => array('type' => 'integer', 'null' => false, 'default' => null, 'unsigned' => true),
+		'port' => array('type' => 'integer', 'null' => false, 'default' => null, 'unsigned' => true),
+		'vlan' => array('type' => 'integer', 'null' => false, 'default' => null, 'length' => 4, 'unsigned' => true),
+		'up_down' => array('type' => 'boolean', 'null' => true, 'default' => '0'),
+		'created' => array('type' => 'datetime', 'null' => true, 'default' => null),
+		'indexes' => array(
+			
+		),
+		'tableParameters' => array('charset' => 'latin1', 'collate' => 'latin1_swedish_ci', 'engine' => 'MyISAM')
+	);
+
+	public $device_attr_oids = array(
+		'id' => array('type' => 'biginteger', 'null' => false, 'default' => null, 'unsigned' => true, 'key' => 'primary'),
+		'sys_object_id' => array('type' => 'string', 'null' => false, 'default' => null, 'length' => 512, 'collate' => 'latin1_swedish_ci', 'charset' => 'latin1'),
+		'name' => array('type' => 'string', 'null' => false, 'default' => null, 'collate' => 'latin1_swedish_ci', 'charset' => 'latin1'),
+		'oid' => array('type' => 'string', 'null' => false, 'default' => null, 'length' => 512, 'collate' => 'latin1_swedish_ci', 'charset' => 'latin1'),
+		'filter' => array('type' => 'text', 'null' => true, 'default' => null, 'collate' => 'latin1_swedish_ci', 'charset' => 'latin1'),
+		'indexes' => array(
+			'PRIMARY' => array('column' => 'id', 'unique' => 1)
+		),
+		'tableParameters' => array('charset' => 'latin1', 'collate' => 'latin1_swedish_ci', 'engine' => 'InnoDB')
+	);
+
+	public $device_configs = array(
+		'id' => array('type' => 'biginteger', 'null' => false, 'default' => null, 'unsigned' => true, 'key' => 'primary'),
+		'device_ip_addr' => array('type' => 'integer', 'null' => false, 'default' => null, 'unsigned' => true, 'key' => 'index'),
+		'config' => array('type' => 'text', 'null' => false, 'default' => null, 'collate' => 'latin1_swedish_ci', 'charset' => 'latin1'),
+		'diff' => array('type' => 'text', 'null' => true, 'default' => null, 'collate' => 'latin1_swedish_ci', 'charset' => 'latin1'),
+		'created' => array('type' => 'datetime', 'null' => true, 'default' => null),
+		'indexes' => array(
+			'PRIMARY' => array('column' => 'id', 'unique' => 1),
+			'device_configs_device_ip_addr_idx' => array('column' => 'device_ip_addr', 'unique' => 0)
+		),
+		'tableParameters' => array('charset' => 'latin1', 'collate' => 'latin1_swedish_ci', 'engine' => 'InnoDB')
+	);
+
+	public $device_logs = array(
+		'id' => array('type' => 'biginteger', 'null' => false, 'default' => null, 'unsigned' => true, 'key' => 'primary'),
+		'device_ip_addr' => array('type' => 'integer', 'null' => false, 'default' => null, 'unsigned' => true, 'key' => 'index'),
+		'timestamp' => array('type' => 'datetime', 'null' => false, 'default' => null, 'key' => 'index'),
+		'fac_sev_mnem' => array('type' => 'string', 'null' => true, 'default' => null, 'key' => 'index', 'collate' => 'latin1_swedish_ci', 'charset' => 'latin1'),
+		'message' => array('type' => 'text', 'null' => true, 'default' => null, 'collate' => 'latin1_swedish_ci', 'charset' => 'latin1'),
+		'indexes' => array(
+			'PRIMARY' => array('column' => 'id', 'unique' => 1),
+			'device_logs_timestamp_idx' => array('column' => 'timestamp', 'unique' => 0),
+			'device_logs_device_ip_addr_idx' => array('column' => 'device_ip_addr', 'unique' => 0),
+			'device_logs_fac_sev_mnem_idx' => array('column' => 'fac_sev_mnem', 'unique' => 0)
+		),
+		'tableParameters' => array('charset' => 'latin1', 'collate' => 'latin1_swedish_ci', 'engine' => 'InnoDB')
+	);
+
+	public $device_neighbors = array(
+		'id' => array('type' => 'biginteger', 'null' => false, 'default' => null, 'unsigned' => true, 'key' => 'primary'),
+		'device_ip_addr' => array('type' => 'integer', 'null' => false, 'default' => null, 'unsigned' => true, 'key' => 'index'),
+		'neighbor_ip_addr' => array('type' => 'integer', 'null' => false, 'default' => null, 'unsigned' => true, 'key' => 'index'),
+		'local_port' => array('type' => 'string', 'null' => true, 'default' => null, 'length' => 75, 'collate' => 'latin1_swedish_ci', 'charset' => 'latin1'),
+		'neighbor_name' => array('type' => 'string', 'null' => true, 'default' => null, 'collate' => 'latin1_swedish_ci', 'charset' => 'latin1'),
+		'neighbor_port' => array('type' => 'string', 'null' => true, 'default' => null, 'length' => 75, 'collate' => 'latin1_swedish_ci', 'charset' => 'latin1'),
+		'neighbor_platform' => array('type' => 'string', 'null' => true, 'default' => null, 'collate' => 'latin1_swedish_ci', 'charset' => 'latin1'),
+		'last_seen' => array('type' => 'datetime', 'null' => true, 'default' => null),
+		'created' => array('type' => 'datetime', 'null' => true, 'default' => null),
+		'updated' => array('type' => 'datetime', 'null' => true, 'default' => null),
+		'indexes' => array(
+			'PRIMARY' => array('column' => 'id', 'unique' => 1),
+			'device_neighbors_device_ip_addr_idx' => array('column' => 'device_ip_addr', 'unique' => 0),
+			'device_neighbors_neighbor_ip_idx' => array('column' => 'neighbor_ip_addr', 'unique' => 0)
+		),
+		'tableParameters' => array('charset' => 'latin1', 'collate' => 'latin1_swedish_ci', 'engine' => 'InnoDB')
+	);
+
+	public $devices = array(
+		'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'unsigned' => true, 'key' => 'primary'),
+		'ip_addr' => array('type' => 'integer', 'null' => false, 'default' => null, 'unsigned' => true, 'key' => 'index'),
+		'sys_object_id' => array('type' => 'string', 'null' => false, 'default' => null, 'length' => 512, 'collate' => 'latin1_swedish_ci', 'charset' => 'latin1'),
+		'name' => array('type' => 'text', 'null' => true, 'default' => null, 'collate' => 'latin1_swedish_ci', 'charset' => 'latin1'),
+		'serial' => array('type' => 'text', 'null' => true, 'default' => null, 'collate' => 'latin1_swedish_ci', 'charset' => 'latin1'),
+		'firmware' => array('type' => 'text', 'null' => true, 'default' => null, 'collate' => 'latin1_swedish_ci', 'charset' => 'latin1'),
+		'model' => array('type' => 'text', 'null' => true, 'default' => null, 'collate' => 'latin1_swedish_ci', 'charset' => 'latin1'),
+		'last_seen' => array('type' => 'datetime', 'null' => true, 'default' => null),
+		'created' => array('type' => 'datetime', 'null' => true, 'default' => null),
+		'updated' => array('type' => 'datetime', 'null' => true, 'default' => null),
+		'indexes' => array(
+			'PRIMARY' => array('column' => 'id', 'unique' => 1),
+			'devices_ip_addr_idx' => array('column' => 'ip_addr', 'unique' => 0)
+		),
+		'tableParameters' => array('charset' => 'latin1', 'collate' => 'latin1_swedish_ci', 'engine' => 'InnoDB')
+	);
+
+	public $metrics = array(
+		'id' => array('type' => 'biginteger', 'null' => false, 'default' => null, 'unsigned' => true, 'key' => 'primary'),
+		'var' => array('type' => 'string', 'null' => false, 'default' => null, 'key' => 'index', 'collate' => 'latin1_swedish_ci', 'charset' => 'latin1'),
+		'val' => array('type' => 'text', 'null' => true, 'default' => null, 'collate' => 'latin1_swedish_ci', 'charset' => 'latin1'),
+		'created' => array('type' => 'datetime', 'null' => true, 'default' => null),
+		'updated' => array('type' => 'datetime', 'null' => true, 'default' => null),
+		'indexes' => array(
+			'PRIMARY' => array('column' => 'id', 'unique' => 1),
+			'dashboard_stats_var_idx' => array('column' => 'var', 'unique' => 0)
+		),
+		'tableParameters' => array('charset' => 'latin1', 'collate' => 'latin1_swedish_ci', 'engine' => 'InnoDB')
+	);
+
+	public $netmanage_logs = array(
+		'id' => array('type' => 'biginteger', 'null' => false, 'default' => null, 'unsigned' => true, 'key' => 'primary'),
+		'timestamp' => array('type' => 'datetime', 'null' => true, 'default' => null),
+		'type' => array('type' => 'string', 'null' => true, 'default' => 'info', 'collate' => 'latin1_swedish_ci', 'charset' => 'latin1'),
+		'message' => array('type' => 'text', 'null' => true, 'default' => null, 'collate' => 'latin1_swedish_ci', 'charset' => 'latin1'),
+		'indexes' => array(
+			'PRIMARY' => array('column' => 'id', 'unique' => 1)
+		),
+		'tableParameters' => array('charset' => 'latin1', 'collate' => 'latin1_swedish_ci', 'engine' => 'InnoDB')
+	);
+
+	public $roles = array(
+		'id' => array('type' => 'biginteger', 'null' => false, 'default' => null, 'unsigned' => true, 'key' => 'primary'),
+		'name' => array('type' => 'string', 'null' => false, 'default' => null, 'collate' => 'latin1_swedish_ci', 'charset' => 'latin1'),
+		'description' => array('type' => 'string', 'null' => false, 'default' => null, 'collate' => 'latin1_swedish_ci', 'charset' => 'latin1'),
+		'updated' => array('type' => 'datetime', 'null' => true, 'default' => null),
+		'created' => array('type' => 'datetime', 'null' => true, 'default' => null),
+		'indexes' => array(
+			'PRIMARY' => array('column' => 'id', 'unique' => 1)
+		),
+		'tableParameters' => array('charset' => 'latin1', 'collate' => 'latin1_swedish_ci', 'engine' => 'InnoDB')
+	);
+
+	public $settings = array(
+		'id' => array('type' => 'biginteger', 'null' => false, 'default' => null, 'unsigned' => true, 'key' => 'primary'),
+		'var' => array('type' => 'string', 'null' => false, 'default' => null, 'key' => 'index', 'collate' => 'latin1_swedish_ci', 'charset' => 'latin1'),
+		'val' => array('type' => 'text', 'null' => true, 'default' => null, 'collate' => 'latin1_swedish_ci', 'charset' => 'latin1'),
+		'description' => array('type' => 'text', 'null' => true, 'default' => null, 'collate' => 'latin1_swedish_ci', 'charset' => 'latin1'),
+		'created' => array('type' => 'datetime', 'null' => true, 'default' => null),
+		'updated' => array('type' => 'datetime', 'null' => true, 'default' => null),
+		'indexes' => array(
+			'PRIMARY' => array('column' => 'id', 'unique' => 1),
+			'setings_var_idx' => array('column' => 'var', 'unique' => 0)
+		),
+		'tableParameters' => array('charset' => 'latin1', 'collate' => 'latin1_swedish_ci', 'engine' => 'InnoDB')
+	);
+
+	public $users = array(
+		'id' => array('type' => 'biginteger', 'null' => false, 'default' => null, 'unsigned' => true, 'key' => 'primary'),
+		'role_id' => array('type' => 'integer', 'null' => false, 'default' => null, 'unsigned' => true),
+		'username' => array('type' => 'string', 'null' => true, 'default' => null, 'length' => 50, 'key' => 'index', 'collate' => 'latin1_swedish_ci', 'charset' => 'latin1'),
+		'password' => array('type' => 'string', 'null' => false, 'default' => null, 'collate' => 'latin1_swedish_ci', 'charset' => 'latin1'),
+		'email' => array('type' => 'string', 'null' => false, 'default' => null, 'collate' => 'latin1_swedish_ci', 'charset' => 'latin1'),
+		'first_name' => array('type' => 'string', 'null' => false, 'default' => null, 'collate' => 'latin1_swedish_ci', 'charset' => 'latin1'),
+		'last_name' => array('type' => 'string', 'null' => false, 'default' => null, 'collate' => 'latin1_swedish_ci', 'charset' => 'latin1'),
+		'updated' => array('type' => 'datetime', 'null' => true, 'default' => null),
+		'created' => array('type' => 'datetime', 'null' => true, 'default' => null),
+		'indexes' => array(
+			'PRIMARY' => array('column' => 'id', 'unique' => 1),
+			'users_username_idx' => array('column' => 'username', 'unique' => 0)
+		),
+		'tableParameters' => array('charset' => 'latin1', 'collate' => 'latin1_swedish_ci', 'engine' => 'InnoDB')
+	);
+
+}
